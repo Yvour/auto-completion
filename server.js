00e9f7a5c0f10c03f5bbcdaefce5b6c212b895f0
Yvour <ivour@rambler.ru>
2018-06-17 19:20:19 +0300

    working example with Wikipedia, maybe needs some more css

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const app = express();
+const fetch = require('node-fetch');
 
 const compiler = webpack(webpackConfig);
+
 
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
@@ -18,8 +20,32 @@ app.use(webpackDevMiddleware(compiler, {
 
 app.use(express.static(__dirname + '/www'));
 
+app.get("/query/:search", function(req, res) {
+
+    const search = req.params.search;
+    const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${search}&utf8=&format=json`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(response => {
+
+        res.set("Content-Type", "application/json");
+        res.send(response.query.search.map(x => x.title));
+      })
+      .catch(e => {
+
+        res.send([]);
+
+      })
+
+
+  }
+
+)
+
+
 const server = app.listen(3000, function() {
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+
 });
--- src/js/AutoCompletion/index.js
+++ src/js/AutoCompletion/index.js
@@ -1,26 +1,68 @@
 import React from 'react';
+import  "./styles.css";
 
-/**
- * A counter button: tap the button to increase the count.
- */
+const NO_RESULTS = [];
+
+
 class AutoCompletion extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    };
-  }
+    constructor() {
+      super();
+      this.state = {
+        count: 0,
+        lastSend: (new Date()).valueOf(),
+        results: NO_RESULTS
+      };
+    }
 
-  render() {
-    return (
-      <div
-        onClick={() => {
-          this.setState({ count: this.state.count + 1 });
-        }}
-      >
-        AutoComplete: {this.state.count}
-      </div>
-    );
-  }
-}
-export default AutoCompletion;
+    getSearchResult(search) {
+      fetch(`query/${search}`)
+        
+      .then(response=>response.json())
+      .then(result => {
+        console.log(result);
+   console.log(this.state);
+console.log('setting state');
+        this.setState(Object.assign(this.state, {
+          results: result
+        }));
+      })
+
+    }
+
+
+    renderSearchResult(arr) {
+console.log('render');
+console.log(arr);
+      if (!Array.isArray(arr)) return null
+      else return (<div className="search-result-container">
+{arr.map((x, i) =>
+        (<div key={i} className = "search-result-item" > {x} </div>))}
+    </div>
+);
+      }
+
+
+      render() {
+        return ( <div onClick = {
+            () => {
+              this.setState({
+                count: this.state.count + 1
+              });
+            }
+          } >
+          AutoComplete: {   this.state.count     } 
+            <input onChange = {
+            e => {
+              const value = e.target.value
+              if (value && value.length)
+              this.getSearchResult(e.target.value);
+              else this.setState(Object.assign(this.state, {results: NO_RESULTS}))
+            }
+          }
+          />
+    {this.renderSearchResult(this.state.results)}
+   </div>
+        );
+      }
+    }
+    export default AutoCompletion;
